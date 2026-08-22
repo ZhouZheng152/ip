290e174a4ec61cb5b6d0f7fefda2b3d175c97fcb
Zhou Zheng <e1484092@u.nus.edu>
2026-08-23 02:08:32 +0800

    Implement Level-1 echo

--- src/main/java/Vega.java
+++ src/main/java/Vega.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Vega {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Vega");
         System.out.println("What can I do for you?");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                String command = scanner.nextLine();
+
+                if (command.equals("bye")) {
+                    break;
+                }
+
+                System.out.println("You said: " + command);
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
